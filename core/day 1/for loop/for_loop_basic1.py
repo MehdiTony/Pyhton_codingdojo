c638a1877914e1191730eda7788c9d33714d60f2
MehdiOmrani <mehditony17@gmail.com>
2023-01-30 16:47:29 +0100

    for loop

---
+++ core/day 1/for loop/for_loop_basic1.py
@@ -0,0 +1,45 @@
+# for i in range(0,150+1):
+    # print(i)
+
+# for i in range(0,1000+1, 5):
+#         print(i)
+
+
+# for i in range(1,101):
+#     if i%5==0:
+#         print("coding")
+#     if i%10==0:
+#         print("codingdojo")
+
+
+
+# for i in range(0,500000):
+#     if i%5==1:
+#         print("coding")
+#     if i%10==1:
+#         print("codingdojo")
+
+# for i in range(2018,0,-4):
+#     print (i)
+
+lowNum=2
+highNum=9
+mult=3
+for i in range(lowNum,highNum+1):
+    if i%mult==0:
+        print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
